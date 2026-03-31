74bdbd5ed574b36dc8ccc374e8d205bb01031ef1
msmoliniec <mikolaj.smoliniec@cyfroweszkoly.pl>
2026-03-31 09:30:06 +0200

    Implement early ExpenseForm.kt

--- app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,8 +18,11 @@ fun ExpenseForm(
     Column(modifier = Modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        WokulskiTextField("Hello!!", WokulskiTextFieldChanged)
-        WokulskiButton("Click me!!!", WokulskiButtonClicked)
+
+        Text(text = "Wydatki")
+        WokulskiTextField("Nowy wydatek", WokulskiTextFieldChanged)
+        Text(text = "Kwota")
+        WokulskiTextField() { }
     }
 }
 
@@ -26,6 +30,9 @@ fun ExpenseForm(
 @Composable
 fun ExpenseFormPreview(){
     MaterialTheme{
-        ExpenseForm()
+        ExpenseForm(
+            TODO(),
+            TODO()
+        )
     }
 }
