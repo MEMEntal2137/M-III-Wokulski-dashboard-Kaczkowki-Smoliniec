aa7fc267f9f3f8c5c0cedd62f87687a0b75e6166
MEMEntal2137 <piotr.kaczkowski@cyfroweszkoly.pl>
2026-03-31 09:30:19 +0200

    Income part1
    Added income form

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,2 +1,43 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun IncomeForm(
+    
+){
+    Column(){
+        Text(
+            text = "Income"
+        )
+        TextField(
+            value="Jakis tekst",
+            onValueChange = {},
+            label = {Text(text = "Wprowadź nazwę przedmiotu:")},
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            modifier = Modifier.align ( Alignment.CenterHorizontally ),
+            onClick = {}) {
+            Text("Prześlij")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IncomeFormPreview(){
+    IncomeForm()
+}
--- app/src/main/java/com/example/wokolskidashboard/ui/theme/Theme.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/theme/Theme.kt
@@ -13,13 +13,15 @@ import androidx.compose.ui.platform.LocalContext
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    background = PurpleGrey40
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = PurpleGrey80
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
