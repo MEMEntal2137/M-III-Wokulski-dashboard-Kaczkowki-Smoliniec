298d3ef05091c1a6420936bf427910184665a332
mikolaj <mikolaj.smoliniec@cyfroweszkoly.pl>
2026-04-09 18:30:53 +0200

    Implement parent-child relations in ExpenseForm.kt, implement ExpenseForm.kt in MainScreen.kt

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,37 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.wokolskidashboard.model.Transaction
+import com.example.wokolskidashboard.ui.components.BalanceHeader
+import com.example.wokolskidashboard.ui.components.ExpenseForm
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier){
+    var expenseName by remember { mutableStateOf("") }
+    var expenseValue by remember { mutableDoubleStateOf(0.0) }
+    var isWasteful by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier, verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        ExpenseForm(
+            expenseName = expenseName,
+            expenseValue = expenseValue,
+            isWasteful = isWasteful,
+            expenseNameChanged = {expenseName = it},
+            expenseValueChanged = {expenseValue = it.toDouble()},
+            isWastefulValueChanged = {isWasteful = it},
+            {
+                val expense = Transaction(1, expenseName, expenseValue, false)
+                println(expense)
+            }
+        )
+    }
+}
--- app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
@@ -2,17 +2,27 @@ package com.example.wokolskidashboard.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ExpenseForm(
-    WokulskiTextFieldChanged: (String) -> Unit,
+    expenseName: String,
+    expenseValue: Double,
+    isWasteful: Boolean,
+    expenseNameChanged: (String) -> Unit,
+    expenseValueChanged: (String) -> Unit,
+    isWastefulValueChanged: (Boolean) -> Unit,
     WokulskiButtonClicked: () -> Unit
 ){
     Column(modifier = Modifier,
@@ -20,9 +30,16 @@ fun ExpenseForm(
         horizontalAlignment = Alignment.CenterHorizontally) {
 
         Text(text = "Wydatki")
-        WokulskiTextField("Nowy wydatek", WokulskiTextFieldChanged)
+        WokulskiTextField("Nowy wydatek", onTextValueChange = expenseNameChanged)
         Text(text = "Kwota")
-        WokulskiTextField() { }
+        WokulskiTextField("", onTextValueChange = expenseValueChanged)
+        Row(modifier = Modifier.padding(top = 16.dp), verticalAlignment = Alignment.CenterVertically) {
+            Text("Wydatek zbyteczny")
+            Switch(modifier = Modifier.padding(10.dp),
+                checked = false, onCheckedChange = isWastefulValueChanged)
+        }
+        Spacer(Modifier.height(10.dp))
+        WokulskiButton("Dodaj wydatek", Clicked = WokulskiButtonClicked)
     }
 }
 
@@ -31,8 +48,13 @@ fun ExpenseForm(
 fun ExpenseFormPreview(){
     MaterialTheme{
         ExpenseForm(
-            TODO(),
-            TODO()
+            expenseName = "",
+            expenseValue = 0.0,
+            isWasteful = false,
+            expenseNameChanged = {},
+            expenseValueChanged = {},
+            isWastefulValueChanged = {},
+            WokulskiButtonClicked = {}
         )
     }
 }
